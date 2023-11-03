28622a4ccb6c5e03140e9d10c6ae0ba869e3b850
Rufo <jmjoenk@gmail.com>
2023-11-03 17:27:54 -0300

    ok
    ok

--- Exercicios/testes/dungeonclick.py
+++ Exercicios/testes/dungeonclick.py
@@ -5,7 +5,8 @@ print('''
       [ 1 ] Matar Monstros em Tela Espremida
       [ 2 ] Mineirar em Tela Espremida
       [ 3 ] Matar Monstros em Tela Cheia
-      [ 5 ] Matar Monstros na Segunda Tela''')
+      [ 5 ] Matar Monstros na Segunda Tela
+      [ 6 ] Mineirar na Segunda Tela''')
 n = int(input('Qual o layout do farm? '))
 
 if n == 1:
@@ -32,3 +33,9 @@ elif n == 5:
         pyautogui.click(2364, 524)
         time.sleep(40)
         pyautogui.click(2308, 524)
+elif n == 6:
+    while True:
+        time.sleep(15)
+        pyautogui.click(2362, 613)
+        time.sleep(15)
+        pyautogui.click(2308, 613)
